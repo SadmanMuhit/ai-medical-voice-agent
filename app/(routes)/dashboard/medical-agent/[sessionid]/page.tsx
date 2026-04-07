1c249c78ed42ea58b61703786f6f60c02da3f5db
sadman muhit <ssmuhit9699@gmail.com>
2026-04-07 18:34:38 +0600

    new work

--- app/(routes)/dashboard/medical-agent/[sessionid]/page.tsx
+++ app/(routes)/dashboard/medical-agent/[sessionid]/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { CircleIcon } from "@phosphor-icons/react";
+import { CircleIcon, PhoneCallIcon } from "@phosphor-icons/react";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { AIDoctorAgents } from "@/shared/list";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 type AIDoctorAgent = (typeof AIDoctorAgents)[number];
 
@@ -30,7 +31,7 @@ const MedicalVoiceAgent = () => {
     setSessionDetails(result.data);
   };
   return (
-    <div>
+    <div className="p-5 border rounded-3xl bg-secondary">
       <div className="flex justify-between items-center">
         <h2 className="p-1 px-2 border rounded-md flex gap-2 items-center">
           <CircleIcon className="w-4 h-4" /> Not Connected
@@ -40,8 +41,17 @@ const MedicalVoiceAgent = () => {
 
      {sessionDetails && 
       <div className="flex items-center flex-col mt-10">
-        <Image src={sessionDetails?.selectedDoctor?.image} alt={sessionDetails?.selectedDoctor?.specialist??""}/>
-        <h2 className="mt-2">{sessionDetails?.selectedDoctor?.specialist}</h2>
+        <Image src={sessionDetails?.selectedDoctor?.image} alt={sessionDetails?.selectedDoctor?.specialist??""} width={120} height={120}
+        className="h-25 w-25 object-cover rounded-full"/>
+        <h2 className="mt-2 text-lg">{sessionDetails?.selectedDoctor?.specialist}</h2>
+        <p className="text-sm text-gray-400">AI Medical Voice Agent</p>
+
+        <div className="mt-32">
+          <h2 className="text-gray-400">Assistant Msg</h2>
+          <h2 className="text-lg">User Msg</h2>
+        </div>
+
+        <Button className="mt-20"><PhoneCallIcon/>Start Call</Button>
       </div>
      }
     </div>
