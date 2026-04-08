eb0ec082edc12d84824a607fa50e87584d4b4bb8
sadman muhit <ssmuhit9699@gmail.com>
2026-04-08 18:18:49 +0600

    new work

--- app/(routes)/dashboard/medical-agent/[sessionid]/page.tsx
+++ app/(routes)/dashboard/medical-agent/[sessionid]/page.tsx
@@ -6,7 +6,8 @@ import React, { useEffect, useState } from "react";
 import { AIDoctorAgents } from "@/shared/list";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-
+import Vapi from '@vapi-ai/web';
+import { FiPhoneOff } from "react-icons/fi";
 type AIDoctorAgent = (typeof AIDoctorAgents)[number];
 
 type SessionDetails = {
@@ -21,6 +22,10 @@ type SessionDetails = {
 const MedicalVoiceAgent = () => {
   const { sessionid } = useParams();
   const [sessionDetails, setSessionDetails] = useState<SessionDetails>();
+  const [callStarted, setCallStarted] =useState(false);
+  const [vapiInstance, setVapiInstance] =useState<any>();
+  const [currentRoll, setCurrentRoll] =useState<string>();
+  const [liveTranscript, setLiveTranscript] =useState<string>();
   useEffect(() => {
     sessionid && GetSessionDetails();
   }, [sessionid]);
@@ -29,12 +34,52 @@ const MedicalVoiceAgent = () => {
     console.log(result.data);
 
     setSessionDetails(result.data);
+  }
+
+  const StartCall=()=>{
+      const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
+      vapi.start(process.env.NEXT_PUBLIC_VAPI_VOICE_ASSISTANT_ID);
+      setVapiInstance(vapi);
+      vapi.on('call-start', () => {console.log('Call started')
+        setCallStarted(true);
+      });
+      vapi.on('call-end', () => {
+        setCallStarted(false);
+        console.log('Call ended')});
+      vapi.on('message', (message) => {
+  if (message.type === 'transcript') {
+    const {role, transcriptType,transcript} = message;
+    console.log(`${message.role}: ${message.transcript}`);
+  }
+});
+
+
+    vapiInstance.on('speech-start', () => {
+      console.log('Assistant started speaking');
+      setCurrentRoll('assistant')
+    });
+    vapiInstance.on('speech-end', () => {
+      console.log('Assistant stopped speaking');
+      setCurrentRoll('user')
+    });
+  }
+
+  const endCall = () => {
+    if (vapiInstance) return;
+    vapiInstance.stop();
+
+    vapiInstance.off('call-start');
+    vapiInstance.off('call-end');
+    vapiInstance.off('message');
+
+    setCallStarted(false);
+    setVapiInstance(null);
   };
   return (
     <div className="p-5 border rounded-3xl bg-secondary">
       <div className="flex justify-between items-center">
         <h2 className="p-1 px-2 border rounded-md flex gap-2 items-center">
-          <CircleIcon className="w-4 h-4" /> Not Connected
+          <CircleIcon className={`w-4 h-4 rounded-full ${callStarted?"bg-green-500":"bg-red-500"}`} />{callStarted?'Connected...':'Not Connected'} 
         </h2>
         <h2 className="font-bold text-xl text-gray-400">00:00</h2>
       </div>
@@ -51,7 +96,9 @@ const MedicalVoiceAgent = () => {
           <h2 className="text-lg">User Msg</h2>
         </div>
 
-        <Button className="mt-20"><PhoneCallIcon/>Start Call</Button>
+        {!callStarted ? <Button className="mt-20" onClick={StartCall}><PhoneCallIcon/>Start Call</Button>
+      : <Button variant={'destructive'} onClick={endCall}><FiPhoneOff />Disconnect</Button>  
+      }
       </div>
      }
     </div>
